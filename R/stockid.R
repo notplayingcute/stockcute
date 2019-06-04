36400266050f4bbf54deab7fb29f003861a0a3bf
notplayingcute <1613104@mail.nankai.edu.cn>
2019-06-04 10:21:22 +0800

    Add files via upload

--- R/stockid.R
+++ R/stockid.R
@@ -4,8 +4,9 @@ stockid <- function(symbol,apikey,interval = "5min",outputsize = "compact"){
     sym <- paste("&symbol=",symbol,sep = "")
     out <- paste("&outputsize=",outputsize,sep = "")
     key <- paste("&apikey=",apikey,sep = "")
+    int <- paste("&interval=",interval,sep = "")
     urls <- "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY"
-    urlss <- paste(urls,sym,out,key,sep = "")
+    urlss <- paste(urls,sym,int,out,key,sep = "")
     t <- GET(urlss,config = list())
     t3 <- fromJSON(content(t, as="text"))
     return(t3)
@@ -13,5 +14,6 @@ stockid <- function(symbol,apikey,interval = "5min",outputsize = "compact"){
   else
     print("error")
 }
+
 
 
